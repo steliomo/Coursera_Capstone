b40641054a4c2ee0a34d1ca3d0a3554138ea6a48
= <=>
2020-04-04 10:34:40 +0200

    Coursera Capstone

---
+++ Coursera Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This capstone is going to be used as the conclusion of Data science professional course at Coursera
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
