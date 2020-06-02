51d641f75a830f7e4148ba5881d72e7614803fe9
Stélio Klésio Adriano Moiane <steliomo@gmail.com>
2020-06-02 20:45:41 +0200

    Mercearias Online - Week 1 - Part-2.
    Mercearias Online - Week 1 - Part-2.

---
+++ Mercearias Online - Week 1 - Part-2.py
@@ -0,0 +1,4 @@
+# Data
+
+The startup pretends to use the grocery data provided by foursquare, such as name, phone number, address, and so on to call or visit some establishments that play on grocery business domain.
+So we will leverage the foursquare API calls do the JSON data and format do a tables using pandas lib.
